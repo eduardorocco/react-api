dfdaffe404cca76a08cbe9f9f21ca70a1e9b5ad2
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-04 15:00:58 +0100

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,120 @@
+import style from './Main.module.css'
+import Card from "../Card/Card"
+import { posts } from '../../data/posts'
+import { useState } from 'react'
+
+const initialFormData = {
+    id: '',
+    title: '',
+    image: '',
+    content: '',
+    tags: '',
+    published: true
+}
+
+export default function Main() {
+    const [publishedPosts, setPublishedPosts] = useState(posts.filter((post) => post.published === true))
+    const [formData, setFormData] = useState(initialFormData)
+
+    function handleFormData(e) {
+        const key = e.target.name;
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+
+        setFormData({
+            ...formData,
+            [key]: value,
+        });
+    }
+
+    function addPost(event) {
+        event.preventDefault()
+
+
+        if (formData.title.trim() === '') return
+
+
+        const newPost = {
+            id: Date.now(),
+            ...formData,
+            tags: formData.tags.split(',').map((tag) => tag.trim())
+        };
+
+
+        setPublishedPosts([...publishedPosts, newPost])
+
+
+        setFormData(initialFormData)
+    }
+
+    function deletePost(id) {
+        setPublishedPosts(publishedPosts.filter(post => post.id !== id))
+    }
+
+    return (
+        <main>
+            <div className="container">
+                <form onSubmit={addPost} className={style.form}>
+                    <h2>Aggiungi un nuovo post</h2>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Titolo del post"
+                        value={formData.title}
+                        onChange={handleFormData}
+                    />
+                    <input
+                        type="text"
+                        name="content"
+                        placeholder="Contenuto"
+                        value={formData.content}
+                        onChange={handleFormData}
+                    />
+                    <input
+                        type="text"
+                        name="image"
+                        placeholder="Url immagine"
+                        value={formData.image}
+                        onChange={handleFormData}
+                    />
+                    <input
+                        type="text"
+                        name="tags"
+                        placeholder="Tags"
+                        value={formData.tags}
+                        onChange={handleFormData}
+                    />
+                    <div className={style.checkbox}>
+                        <input
+                            type="checkbox"
+                            name="published"
+                            checked={formData.published}
+                            onChange={handleFormData}
+                        />
+                        <label htmlFor="checkbox">Pubblica</label>
+                    </div>
+
+                    
+                    <input className={style.submit} type="submit" />
+                    
+                    
+                </form>
+            </div>
+            <div className="container">
+                <div className={style.row}>
+                    {publishedPosts.map((post) => (
+                        <div key={post.id} className="col-6">
+                            <Card
+                                title={post.title}
+                                content={post.content}
+                                tags={post.tags}
+                                image={post.image}
+                                onDelete={() => deletePost(post.id)} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
+}
+
+
